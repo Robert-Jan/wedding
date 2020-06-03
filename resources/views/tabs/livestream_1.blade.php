d2f585d3ec0678f2b15f0426f75f6d81479b5e42
Robert-Jan van der Elst <rjvdelst@gmail.com>
2020-06-03 20:53:10 +0200

    Added Pexip explanation

--- resources/views/tabs/livestream_1.blade.php
+++ resources/views/tabs/livestream_1.blade.php
@@ -1,13 +1,27 @@
 <div class="mx-auto w-3/4">
-    <iframe src="https://www.youtube.com/embed/ATXSYekaRbU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
-        class="h-128 w-full rounded-t-md"></iframe>
+    <div class="flex h-64 bg-gray-600 rounded-t-md bg-center bg-cover" style="background-image: url('/images/gemeentehuis.jpeg');">
+        <a href="https://www.pexip.com/apps" target="_blank" 
+           class="flex flex-col justify-center content-center w-full h-full">
+           <div class="w-80 mx-auto py-6 px-12 text-center bg-white bg-opacity-75 rounded-full">
+                Download de Pexip app:
+                <img src="/images/pexip.png" class="w-64 mt-2">
+            </div>
+        </a>
+    </div>
     <div class="w-full p-6 text-gray-200 bg-gray-600 rounded-b-md">
         <h1 style="font-family: special" class="text-4xl">Huwelijksvoltrekking</h1>
         <p class="w-2/3">
-            Hier kunt u de huwelijksvoltrekking live volgen. Deze begint om 11:00 uur en vindt plaats in het gemeentehuis te Capelle aan den IJssel. 
-            De huwelijksvoltrekking wordt bijgewoond door beide ouderparen. 
+            Met Pexip kunt u de huwelijksvoltrekking live volgen. Deze begint om 11:00 uur en vindt plaats in het gemeentehuis te Capelle aan den IJssel. 
+            De huwelijksvoltrekking wordt bijgewoond door beide ouderparen en de broers en zussen.
         </p>
+        <ol class="list-decimal list-inside mt-4">
+          <li class="mb-1">Download de Pexip software (<a href="https://static.videxio.net/mmv/prod/mmv-3.0.14-818003223-win32-ia32.msi" target="_blank" class="underline">Windows</a>, <a href="https://static.videxio.net/mmv/prod/mmv-3.0.14-818003223-darwin-x64.pkg" target="_blank" class="underline">MacOS</a>, <a href="https://apps.apple.com/us/app/pexip-my-meeting-video/id1018923867" target="_blank" class="underline">iOS</a>, <a href="https://play.google.com/store/apps/details?id=com.mymeetingvideo.app&hl=en&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1" target="_blank" class="underline">Android</a>)</li>
+          <li class="mb-1">Klik op ‘Join room as guest’</li>
+          <li class="mb-1">Vul je volledige naam in bij ‘display name’</li>
+          <li class="mb-1">Voer bij ‘Video address or conference code’ de code: <span class="font-bold">57495974</span> en klik vervolgens op ‘Join room’</li>
+          <li class="mb-1">(Mobile App): Klik op "video call"</li>
+          <li class="mb-1">Vul de PIN in: <span class="font-bold">5456</span> en klik op ‘Join room’</li>
+          <li class="font-bold">BELANGRIJK: Zet de webcam en microfoon uit!</li>
+        </ol>
     </div>
 </div>
-
-
